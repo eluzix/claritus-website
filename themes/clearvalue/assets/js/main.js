a79add6e13190e708ec06f0a52d2b129ec647b15
nastya <vesna4413@gmail.com>
2020-02-28 11:08:05 +0200

    add fixed navbar and functions to show button on scroll

--- themes/clearvalue/assets/js/main.js
+++ themes/clearvalue/assets/js/main.js
@@ -2,7 +2,7 @@ const app = {
     earlyAccessClick(e){
         const parent = e.target.closest('.early-access');
         const input = parent.querySelector('input.input');
-        
+
         console.log('!!!!!!!!!!!!!!!', e, input.value)
     },
 };
@@ -13,4 +13,29 @@ window.addEventListener('load', function(){
         buttons[i].addEventListener('click', app.earlyAccessClick)
     }
 
-})
+});
+
+function checkHeroVisibility() {
+    let heroElement = document.querySelector('.hero');
+    if (!heroElement) {
+        return false;
+    }
+    let rect = heroElement.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    let headerButton = document.querySelector('.cv-header .early-access-button');
+    if (isVisible) {
+        headerButton.classList.add("d-none");
+    } else {
+        headerButton.classList.remove("d-none");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOMContentLoaded');
+    window.addEventListener('scroll', checkHeroVisibility);
+}, false);
+
