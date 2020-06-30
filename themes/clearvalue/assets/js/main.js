e0fd0f0e6d3c90b0810e133e055ec299acf17e89
nastya <vesna4413@gmail.com>
2020-06-30 11:51:43 +0300

    add slider for block

--- themes/clearvalue/assets/js/main.js
+++ themes/clearvalue/assets/js/main.js
@@ -1,3 +1,5 @@
+const sliderIntervalTime = 5000; // 5sec
+
 const app = {
     earlyAccessClick(e){
         const parent = e.target.closest('.early-access');
@@ -10,6 +12,44 @@ window.addEventListener('load', function(){
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', app.earlyAccessClick)
     }
+
+
+
+
+    var slider = new KeenSlider("#main-slider", {
+        loop: true,
+        created: function(instance) {
+            let timerId = setInterval(() => {
+                document
+                    .getElementById("arrow-right").click()
+            }, sliderIntervalTime);
+            document
+                .getElementById("arrow-left")
+                .addEventListener("click", function() {
+                    instance.prev();
+                });
+
+            document
+                .getElementById("arrow-right")
+                .addEventListener("click", function() {
+                    instance.next();
+                });
+            var dots_wrapper = document.getElementById("dots");
+            var slides = document.querySelectorAll(".keen-slider__slide");
+            slides.forEach(function(t, idx) {
+                var dot = document.createElement("button");
+                dot.classList.add("dot");
+                dots_wrapper.appendChild(dot);
+                dot.addEventListener("click", function() {
+                    instance.moveToSlide(idx);
+                });
+            });
+            updateClasses(instance);
+        },
+        slideChanged(instance) {
+            updateClasses(instance);
+        },
+    });
 
 });
 
@@ -34,6 +74,26 @@ function checkHeroVisibility() {
     }
 
 }
+
+function updateClasses(instance) {
+    var slide = instance.details().relativeSlide;
+    var arrowLeft = document.getElementById("arrow-left");
+    var arrowRight = document.getElementById("arrow-right");
+    slide === 0
+        ? arrowLeft.classList.add("arrow--disabled")
+        : arrowLeft.classList.remove("arrow--disabled");
+    slide === instance.details().size - 1
+        ? arrowRight.classList.add("arrow--disabled")
+        : arrowRight.classList.remove("arrow--disabled");
+
+    var dots = document.querySelectorAll(".dot");
+    dots.forEach(function(dot, idx) {
+        idx === slide
+            ? dot.classList.add("dot--active")
+            : dot.classList.remove("dot--active");
+    });
+}
+
 
 document.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('scroll', checkHeroVisibility);
