f0ebc72f98aaa43b12a021a5f8885104e6fde4c5
nastya <vesna4413@gmail.com>
2020-08-25 15:49:57 +0300

    fix slider layout

--- themes/clearvalue/assets/js/main.js
+++ themes/clearvalue/assets/js/main.js
@@ -145,6 +145,18 @@ async function getEarlyAccess(elem) {
     }
 }
 
+function coloredTitle() {
+    let pageTitle = document.querySelector('h1.title');
+
+    if (pageTitle && pageTitle.innerHTML) {
+        let word_array = pageTitle.innerHTML.split(/\s+/); // split on spaces
+        let last_word = word_array.pop();             // pop the last word
+        let first_part = word_array.join(' ');        // rejoin the first words together
+
+
+        pageTitle.innerHTML = [first_part, ' <span class="text-orange">', last_word, '</span>'].join('')
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('.early-access .button');
@@ -154,4 +166,5 @@ document.addEventListener('DOMContentLoaded', function(){
 
     setupMenuEvents();
     setupSlider();
+    coloredTitle();
 });
