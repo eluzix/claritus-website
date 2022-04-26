54918b9022dca6d551baddcd361931750cc9426e
Maks Behter <behter_mo@groupbwt.com>
2022-04-26 21:34:39 +0300

    feat: footer / header scroll

--- themes/clearvalue/assets/js/app.js
+++ themes/clearvalue/assets/js/app.js
@@ -11,9 +11,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
         adaptiveHeight: false,
     });
 
+    // header
+    let scrollPosition = window.scrollY;
+    const header = document.querySelector('.header-section');
+
+    window.addEventListener('scroll', function () {
+        scrollPosition = window.scrollY;
+        if (scrollPosition > 50) {
+            header.classList.add('header--fixed');
+        } else {
+            header.classList.remove('header--fixed');
+        }
+    });
 
     // Faq
-
     const faqButtons = document.querySelectorAll('.faq__question');
 
     faqButtons.forEach((button) => {
