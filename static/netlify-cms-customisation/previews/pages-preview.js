eec0dc408fd53b6b9887b72a916bf7fd88858950
Maks Behter <behter_mo@groupbwt.com>
2022-05-19 22:47:02 +0300

    feat: security base preview

--- static/netlify-cms-customisation/previews/pages-preview.js
+++ static/netlify-cms-customisation/previews/pages-preview.js
@@ -108,4 +108,40 @@ const AboutPreview = createClass({
   },
 });
 
+const SecurityPreview = createClass({
+    render: function () {
+        const { entry, widgetFor, widgetsFor } = this.props;
+
+        return h(
+          "div",
+          { className: "container" },
+
+          h(
+            "section",
+            {},
+
+            h(
+              "div",
+              { className: "row align-center" },
+              h(
+                "div",
+                { className: "col-5" },
+                h("h1", {}, entry.getIn(["data", "title"])),
+                h("p", {}, entry.getIn(["data", "description"]))
+              ),
+
+              h(
+                "div",
+                { className: "col-7" },
+                h("img", { src: entry.getIn(["data", "heroImgFullHd"]) })
+              )
+            )
+          ),
+
+
+        );
+    },
+});
+
 CMS.registerPreviewTemplate("about-us", AboutPreview);
+CMS.registerPreviewTemplate("about-us", SecurityPreview);
